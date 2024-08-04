148c275d6a914bc89eb224aec9d05570969a0afc
rapier-rodent <rapierodent@gmail.com>
2024-08-04 22:04:40 +0530

    Update VedicAstroAPI.py

--- VedicAstroAPI.py
+++ VedicAstroAPI.py
@@ -1,13 +1,8 @@
-import logging
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from vedicastro import VedicAstro, horary_chart
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -55,75 +50,62 @@ async def read_root():
 
 @app.post("/get_all_horoscope_data")
 async def get_chart_data(input: ChartInput):
-    try:
-        horoscope = VedicAstro.VedicHoroscopeData(
-            input.year, input.month, input.day, input.hour, input.minute,
-            input.second, input.utc, input.latitude, input.longitude,
-            input.ayanamsa, input.house_system
-        )
-        chart = horoscope.generate_chart()
-        planets_data = horoscope.get_planets_data_from_chart(chart)
-        houses_data = horoscope.get_houses_data_from_chart(chart)
-        planet_significators = horoscope.get_planet_wise_significators(planets_data, houses_data)
-        planetary_aspects = horoscope.get_planetary_aspects(chart)
-        house_significators = horoscope.get_house_wise_significators(planets_data, houses_data)
-        vimshottari_dasa_table = horoscope.compute_vimshottari_dasa(chart)
-        consolidated_chart_data = horoscope.get_consolidated_chart_data(planets_data=planets_data, houses_data=houses_data, return_style=input.return_style)
+    """ Generates all data for a given time and location, based on the selected ayanamsa & house system """
+    horoscope = VedicAstro.VedicHoroscopeData(
+        input.year, input.month, input.day, input.hour, input.minute,
+        input.second, input.utc, input.latitude, input.longitude,
+        input.ayanamsa, input.house_system
+    )
+    
+    # Retrieve the ayanamsa value
+    ayanamsa_value = horoscope.get_ayanamsa()
 
-        birth_time_ist = f"{input.hour + 5}:{input.minute} IST"
-        krishnamurti_ayanamsa = VedicAstro.calculate_ayanamsa(input.year, input.month, input.day, "Krishnamurti")
+    chart = horoscope.generate_chart()
+    planets_data = horoscope.get_planets_data_from_chart(chart)
+    houses_data = horoscope.get_houses_data_from_chart(chart)
+    planet_significators = horoscope.get_planet_wise_significators(planets_data, houses_data)
+    planetary_aspects = horoscope.get_planetary_aspects(chart)
+    house_significators = horoscope.get_house_wise_significators(planets_data, houses_data)
+    vimshottari_dasa_table = horoscope.compute_vimshottari_dasa(chart)
+    consolidated_chart_data = horoscope.get_consolidated_chart_data(planets_data=planets_data, houses_data=houses_data, return_style=input.return_style)
 
-        return {
-            "birth_time_ist": birth_time_ist,
-            "ayanamsa": input.ayanamsa,
-            "krishnamurti_ayanamsa": krishnamurti_ayanamsa,
-            "planets_data": [planet._asdict() for planet in planets_data],
-            "houses_data": [house._asdict() for house in houses_data],
-            "planet_significators": planet_significators,
-            "planetary_aspects": planetary_aspects,
-            "house_significators": house_significators,
-            "vimshottari_dasa_table": vimshottari_dasa_table,
-            "consolidated_chart_data": consolidated_chart_data
-        }
-    except Exception as e:
-        logger.error(f"Error generating chart data: {e}")
-        return {
-            "error": "Internal Server Error",
-            "details": str(e)  # Include the exception message for troubleshooting
-        }
+    return {
+        "ayanamsa_value": ayanamsa_value,  # Include the ayanamsa value in the response
+        "planets_data": [planet._asdict() for planet in planets_data],
+        "houses_data": [house._asdict() for house in houses_data],
+        "planet_significators": planet_significators,
+        "planetary_aspects": planetary_aspects,
+        "house_significators": house_significators,
+        "vimshottari_dasa_table": vimshottari_dasa_table,
+        "consolidated_chart_data": consolidated_chart_data
+    }
 
 @app.post("/get_all_horary_data")
 async def get_horary_data(input: HoraryChartInput):
-    try:
-        matched_time, vhd_hora_houses_chart, houses_data = horary_chart.find_exact_ascendant_time(
-            input.year, input.month, input.day, input.utc, input.latitude,
-            input.longitude, input.horary_number, input.ayanamsa
-        )
-        vhd_hora = VedicAstro.VedicHoroscopeData(
-            input.year, input.month, input.day, input.hour, input.minute,
-            input.second, input.utc, input.latitude, input.longitude,
-            input.ayanamsa, input.house_system
-        )
-        vhd_hora_planets_chart = vhd_hora.generate_chart()
-        planets_data = vhd_hora.get_planets_data_from_chart(vhd_hora_planets_chart, vhd_hora_houses_chart)
-        planet_significators = vhd_hora.get_planet_wise_significators(planets_data, houses_data)
-        planetary_aspects = vhd_hora.get_planetary_aspects(vhd_hora_planets_chart)
-        house_significators = vhd_hora.get_house_wise_significators(planets_data, houses_data)
-        vimshottari_dasa_table = vhd_hora.compute_vimshottari_dasa(vhd_hora_planets_chart)
-        consolidated_chart_data = vhd_hora.get_consolidated_chart_data(planets_data=planets_data, houses_data=houses_data, return_style=input.return_style)
+    """ Generates all data for a given horary number, time and location as per KP Astrology system """
+    matched_time, vhd_hora_houses_chart, houses_data = horary_chart.find_exact_ascendant_time(
+        input.year, input.month, input.day, input.utc, input.latitude,
+        input.longitude, input.horary_number, input.ayanamsa
+    )
+    vhd_hora = VedicAstro.VedicHoroscopeData(
+        input.year, input.month, input.day, input.hour, input.minute,
+        input.second, input.utc, input.latitude, input.longitude,
+        input.ayanamsa, input.house_system
+    )
+    vhd_hora_planets_chart = vhd_hora.generate_chart()
+    planets_data = vhd_hora.get_planets_data_from_chart(vhd_hora_planets_chart, vhd_hora_houses_chart)
+    planet_significators = vhd_hora.get_planet_wise_significators(planets_data, houses_data)
+    planetary_aspects = vhd_hora.get_planetary_aspects(vhd_hora_planets_chart)
+    house_significators = vhd_hora.get_house_wise_significators(planets_data, houses_data)
+    vimshottari_dasa_table = vhd_hora.compute_vimshottari_dasa(vhd_hora_planets_chart)
+    consolidated_chart_data = vhd_hora.get_consolidated_chart_data(planets_data=planets_data, houses_data=houses_data, return_style=input.return_style)
 
-        return {
-            "planets_data": [planet._asdict() for planet in planets_data],
-            "houses_data": [house._asdict() for house in houses_data],
-            "planet_significators": planet_significators,
-            "planetary_aspects": planetary_aspects,
-            "house_significators": house_significators,
-            "vimshottari_dasa_table": vimshottari_dasa_table,
-            "consolidated_chart_data": consolidated_chart_data
-        }
-    except Exception as e:
-        logger.error(f"Error generating horary data: {e}")
-        return {
-            "error": "Internal Server Error",
-            "details": str(e)  # Include the exception message for troubleshooting
-        }
+    return {
+        "planets_data": [planet._asdict() for planet in planets_data],
+        "houses_data": [house._asdict() for house in houses_data],
+        "planet_significators": planet_significators,
+        "planetary_aspects": planetary_aspects,
+        "house_significators": house_significators,
+        "vimshottari_dasa_table": vimshottari_dasa_table,
+        "consolidated_chart_data": consolidated_chart_data
+    }
